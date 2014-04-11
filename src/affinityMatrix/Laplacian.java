a7c4de361713af38c57c31cf0053ff60ee303e5c
BickfordA <aidan.bickford@gmail.com>
2014-04-11 12:36:46 -0600

    Creation of Laplacian and eigenvectors
    Need to work on function to select the largest eiginvectors

--- src/affinityMatrix/Laplacian.java
+++ src/affinityMatrix/Laplacian.java
@@ -2,6 +2,7 @@ package affinityMatrix;
 
 import Jama.EigenvalueDecomposition;
 import Jama.Matrix;
+import Jama.*;
 import spectralClustering.TunableParameters;
 
 public class Laplacian {
@@ -22,9 +23,7 @@ public class Laplacian {
 		 */
 		
 		
-		
-		
-		Matrix laplacianMatrix = new Matrix(laplacian);
+		Matrix laplacianMatrix = invSqrtSym(diagonal).times(affinity).times(invSqrtSym(diagonal)); //new Matrix(laplacian);
 		
 		
 		Matrix eiginValues = laplacianMatrix.eig().getV();
@@ -32,7 +31,40 @@ public class Laplacian {
 		
 		
 	}
+    /**
+     * Should be included in jama?
+     * 
+     * pulled from:
+     * http://www.openimaj.org/apidocs/src-html/org/openimaj/math/matrix/MatrixUtils.html
+     * 
+     * additional info:
+     * http://www.openimaj.org/openimaj-core-libs/core-math/apidocs/org/openimaj/math/matrix/MatrixUtils.html#invSqrtSym(Jama.Matrix)
+     * 
+     * Compute the inverse square root, X, of the symmetric matrix A; A^-(1/2)
+     * 
+     * @param matrix
+     *            the symmetric matrix
+     * @return the inverse sqrt of the matrix
+     */
+    public static Matrix invSqrtSym(Matrix matrix) {
+            // A = V*D*V'
+            final EigenvalueDecomposition evd = matrix.eig();
+            final Matrix v = evd.getV();
+            final Matrix d = evd.getD();
+
+            // sqrt of cells of D and store in-place
+            for (int r = 0; r < d.getRowDimension(); r++) {
+                for (int c = 0; c < d.getColumnDimension(); c++) {
+                       if (d.get(r, c) > 0)
+                                d.set(r, c, 1 / Math.sqrt(d.get(r, c)));
+               }
+        }
+
+        return v.times(d).times(v.transpose());
+	}
 	
+    
+    
 	public double[][] largestEigenVectors(int numberOfEiginVectors ){
 		double topEVectors[][] = new double[numberOfEiginVectors][_dataSize];
 		
